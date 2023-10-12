939a1428e556fa0be86d7c0ec446b0dec25260b9
saadMeddiche <saadmeddiche2004201@gmail.com>
2023-10-12 19:54:37 +0100

    Make a TransactionHelper Class and fix the problem of diffrent version of jdk in compile time and in run time

--- gather-grid-app/src/main/java/com/example/User.java
+++ gather-grid-app/src/main/java/com/example/User.java
@@ -1,9 +1,9 @@
 package com.example;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
 
 @Entity
 public class User {
@@ -18,8 +18,7 @@ public class User {
     public User() {
     }
 
-    public User(Long id, String username, String password) {
-        this.id = id;
+    public User(String username, String password) {
         this.username = username;
         this.password = password;
     }
